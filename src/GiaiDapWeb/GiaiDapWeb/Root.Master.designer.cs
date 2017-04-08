ab201e4488141ce94ed072964ec211bea461b59b
Thanh Dam <thanhdam2411@gmail.com>
2017-04-08 15:03:39 +0700

    170408 15:03

--- src/GiaiDapWeb/GiaiDapWeb/Root.Master.designer.cs
+++ src/GiaiDapWeb/GiaiDapWeb/Root.Master.designer.cs
@@ -11,14 +11,5 @@ namespace GiaiDapWeb.Master_Page {
     
     
     public partial class Root {
-        
-        /// <summary>
-        /// body control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder body;
     }
 }
